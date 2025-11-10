a0067fd20b288ec026b3ebd7bf71f576d5e05da6
Michael <mthomson9322@conestogac.on.ca>
2025-11-10 13:13:12 -0500

    Implement user registration functionality with validation and error handling

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,42 @@
 const {sqlPool} = require('../config/database');
 
 
-
+//register user 
 const registerUser = async (req, res) => {
-    
+    try {
+        const {firstName, lastName,email, password} = req.body;
+
+        // ensure all fields are provided
+        if(!firstName || !lastName || !email || !password){
+            return res.status(400).json({message: "All fields are required"});  
+        }
+        //ensure email field has @
+        if(!email.includes('@')){
+            return res.status(400).json({message: "Invalid email address"});
+        }
+
+       // check if user already exists by email
+       const [existingUser] = await sqlPool.query('SELECT * FROM users WHERE email = ?', [email]);
+       if(existingUser.length > 0){
+        return res.status(409).json({message: "Account already exists with this email"});
+       }
+
+       // insert new user into database
+       const [result] = await sqlPool.query(
+        'insert into users (firstName, lastName, email, password) values (?, ?, ?, ?)',
+        [firstName, lastName, email, password]
+     );
+        
+        res.status(201).json({message:"User registered successfully"});
+
+    } catch (error) {
+        console.error('Registration error:', error);
+        res.status(500).json({message: "Error"});
+        
+    }
 }
 
+//login user
 const loginUser = async (req, res) => {
     
 }
