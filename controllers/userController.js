dcca753361589049add65293952c198314359226
Michael <mthomson9322@conestogac.on.ca>
2025-11-11 11:02:21 -0500

    Implemented login user and validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,6 +39,34 @@ const registerUser = async (req, res) => {
 //login user
 const loginUser = async (req, res) => {
     
+    try{
+        const {email,password} = req.body;
+
+        // ensure all fields are provided
+        if(!email || !password){
+            return res.status(400).json({message: "Email and password are required"});
+        }
+
+        // check if user exists by email
+        const [userResult] = await sqlPool.query('SELECT * FROM users WHERE email = ?',[email]);
+        if(userResult.length === 0){
+            return res.status(404).json({message: "No account found with this email"});
+        }
+        const user = userResult[0];
+
+        //check if passwords match
+        if(user.password !== password){
+            return res.status(401).json({message:"Incorrect password"});
+        }
+
+        //successful login
+        res.status(200).json({message:"Login successful"})
+
+    
+    }catch(error){
+        console.error("Login errror: ",error);
+        res.status(500).json({message:"Error"});
+    }
 }
 
 
