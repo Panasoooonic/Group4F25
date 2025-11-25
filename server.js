51f60ce8678a991138fc69fe05784b9d2edfdcdc
kavatess <lacxingau9a2pct@gmail.com>
2025-11-25 12:31:14 -0500

    Fix: cors err

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 const express = require("express");
-const { sqlPool } = require("./config/database");
-const userRoutes = require('./routes/userRoutes.js');
-const tripRoutes = require('./routes/tripRoutes.js');
-const tripSummaryRoutes = require('./routes/tripSummaryRoutes.js');
-const vehicleRoutes = require('./routes/vehicleRoutes.js');
-const tripEventRoutes = require('./routes/tripEventRoutes.js');
+const userRoutes = require("./routes/userRoutes.js");
+const tripRoutes = require("./routes/tripRoutes.js");
+const tripSummaryRoutes = require("./routes/tripSummaryRoutes.js");
+const vehicleRoutes = require("./routes/vehicleRoutes.js");
+const tripEventRoutes = require("./routes/tripEventRoutes.js");
 const app = express();
+const cors = require("cors");
 const port = 3000;
 
-app.use(express.json());
+// Allow requests from http://localhost:8081
+app.use(
+  cors({
+    origin: "http://localhost:8081",
+  })
+);
 
+app.use(express.json());
 
 app.use("/api", userRoutes);
 app.use("/api/trip", tripRoutes);
 app.use("/api/summary", tripSummaryRoutes);
 app.use("/api/vehicle", vehicleRoutes);
 app.use("/api/events", tripEventRoutes);
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
